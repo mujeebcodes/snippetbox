c66467c45c23f9daa409bef404e188415dc9ea38
mujeebcodes <mailmujeeb@icloud.com>
2024-12-07 13:21:01 +0000

    created user model and implemented authentication with sessions

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,16 +10,22 @@ func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-    mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-    dynamic := alice.New(app.sessionManager.LoadAndSave)
+	dynamic := alice.New(app.sessionManager.LoadAndSave)
 
-   mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
-    mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
-    mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
-    mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
+	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
+	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
+	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
+	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 
-    standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
+	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
+	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
+	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
+	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.userLogoutPost))
 
-    return standard.Then(mux)
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+
+	return standard.Then(mux)
 }
